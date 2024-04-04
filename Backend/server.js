d0c4c9c31ee53abfb35b7161df4ecd14eba4b725
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-04-04 09:52:41 +0530

    Update Server

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 const APP_PORT = process.env.PORT || 8000;
 
 //Database Connection
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -19,11 +19,15 @@ db.once('open', () => {
 global.appRoot = path.resolve(__dirname);
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
 app.use('/uploads', express.static('uploads'));
 app.use('/audiobook', express.static('audiobook'));
+app.use("/", (req, res) => {
+    res.send(`
+    <h1>Welcome to Deviantika App Server🚀</h1>`);
+});
 
 app.use(errorHandler);
-app.listen(APP_PORT, ()=> console.log(`Listening on port ${APP_PORT}.`));
+app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT}.`));
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  Run the following command on your device
  1. Go to the App folder.
  2. Open the terminal and type "flutter run" then press enter to start the project on your device.
- 3. Wait up to 30 seconds because this can cause a response delay for the first request that comes in after a period of web service inactivity.
+ 3. Wait up to 60 seconds or more because this can cause a response delay for the first request that comes in after a period of web service inactivity.
